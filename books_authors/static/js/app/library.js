be24261f5de53fb92fde2aaf3e55c15418f2d8f9
Denis Olehov <denolehov@gmail.com>
2015-07-22 17:17:27 +0300

    updated angular headers and csrf configs for django

--- books_authors/static/js/app/library.js
+++ books_authors/static/js/app/library.js
@@ -1,3 +1,10 @@
 'use strict';
 
 var library = angular.module('libraryApp', []);
+
+library
+    .config(function ($httpProvider) {
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    });
