d6a05bc72a643e369c5e4388084964c3d36301ed
Denis Olehov <denolehov@gmail.com>
2015-07-22 17:28:22 +0300

    added author form and view

--- books_authors/library/views.py
+++ books_authors/library/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .forms import AuthorForm
+
+
+class AddAuthorView(TemplateView):
+    template_name = 'add_author_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AddAuthorView, self).get_context_data(**kwargs)
+        context.update(add_author_form=AuthorForm)
+
+        return context
