21f105e3f22bf6edb67eb047a41ae9e4a1e7a992
Denis Olehov <denolehov@gmail.com>
2015-07-23 16:31:03 +0300

    added views and urls for books, slight refactor of author view

--- books_authors/library/urls.py
+++ books_authors/library/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from .views import AuthorsView
+from .views import AuthorsView, BooksView
 
 urlpatterns = [
-    url(r'^$', AuthorsView.as_view(), name='authors'),
+    url(r'^$', AuthorsView.as_view()),
+    url(r'^authors/$', AuthorsView.as_view(), name='authors'),
+    url(r'^books/$', BooksView.as_view(), name='books'),
 ]
--- books_authors/library/views.py
+++ books_authors/library/views.py
@@ -1,14 +1,9 @@
 from django.views.generic import TemplateView
 
-from .forms import AuthorForm
-
 
 class AuthorsView(TemplateView):
     template_name = 'library/authors.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(AuthorsView, self).get_context_data(**kwargs)
-        context.update(add_author_form=AuthorForm())
-        print(context)
 
-        return context
+class BooksView(TemplateView):
+    template_name = 'library/books.html'
