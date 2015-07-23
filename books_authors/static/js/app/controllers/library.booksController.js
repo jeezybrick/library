03681137bc27c97639ee1455bea79d772ec5fa80
Denis Olehov <denolehov@gmail.com>
2015-07-23 17:11:06 +0300

    updated books template and controller

--- books_authors/static/js/app/controllers/library.booksController.js
+++ books_authors/static/js/app/controllers/library.booksController.js
@@ -3,17 +3,21 @@
 library.controller('booksCtrl', function ($scope, $http) {
 
     // Initialization
-    $scope.title = '';
-    $scope.author = '';
-    $scope.annotation = '';
-    $scope.genre = [];
-    $scope.slug = '';
+    $scope.books = [];
+    //$scope.title = '';
+    //$scope.author = '';
+    //$scope.annotation = '';
+    //$scope.genre = [];
+    //$scope.slug = '';
 
     // GET list of available books
     $http
         .get('/api/books/?format=json')
         .success(function (out_data) {
-            console.log(JSON.stringify(out_data));
+            for (var i = 0; i < out_data.length; i++) {
+                $scope.books.push(out_data[i]);
+                console.log(out_data[i]);
+            }
         });
 
 });
