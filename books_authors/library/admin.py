a43afb1da86bccdbfcb74917c78b631bade6d52e
banksee <denolehov@gmail.com>
2015-09-16 10:47:39 +0300

    admin total update

--- books_authors/library/admin.py
+++ books_authors/library/admin.py
@@ -5,7 +5,8 @@ from .models import Author, Book, Genre, Review, Rating
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', )
+    search_fields = ('name', )
 
 
 @admin.register(Book)
@@ -17,14 +18,17 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', )
+    search_fields = ('name', )
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('book', 'user', 'text')
+    search_fields = ('book', 'user', 'text')
 
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'value')
     search_fields = ('book__title', 'user__username', )
